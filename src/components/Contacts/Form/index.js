d9b7b4132e0b636418f335ec285a44c946fff0ec
Reyyen cuneyt <rrayyan213@gmail.com>
2024-07-30 14:25:50 +0300

    second commit

--- src/components/Contacts/Form/index.js
+++ src/components/Contacts/Form/index.js
@@ -1,11 +1,42 @@
-import React from 'react'
+import {useState,useEffect} from 'react'
 
-function Form() {
+function Form(addContacts,contacts) {
+    const [form,setForm]=useState({fullname:"",phone_number:""})
+    useEffect(()=>{
+        setForm({fullname:"",phone_number:""})
+    },[contacts])
+    const onChangeInput=(e)=>{
+        setForm({...form,[e.target.name]:e.target.value})
+        //... noktanin anlami bir once deger getir
+    };
+    const onSubmit=(e)=>{
+        e.preventDefault();
+
+        
+        if(form.fullname ==="" || form.phone_number ===""){
+            return false;
+        }
+
+        addContacts(...contacts,form)
+        
+    }
   return (
-    <div>
-      
-    </div>
+    <form onSubmit={onSubmit}>
+      <div>
+        <input name='fullname' 
+               placeholder='full Name'
+               value={form.fullname}
+               onChange={onChangeInput}></input>
+      </div>
+      <div><input name='phone_number'
+                  placeholder='phone Number' 
+                  value={form.phone_number}
+                  onChange={onChangeInput}></input></div>
+      <div><button>Add</button></div>
+    </form>
+    
   )
 }
 
 export default Form
+
